e9a004a851f2d53abb9f7809f53087621a1c3bf4
Alec Elton <alec.elton@gmail.com>
2013-02-10 13:47:29 -0600

    Basic identification proof of concept

--- src/jump/NewClients.py
+++ src/jump/NewClients.py
@@ -2,3 +2,8 @@ import logging
 from jsonrpc import RpcMethod
 
 log=logging.getLogger(__name__)
+
+@RpcMethod("identify")
+def doIdentify(cl, message, username, password):
+	log.debug("Client %s identifying with u=%s, p=%s", cl, username, password)
+	cl.reply(message, result="OK")
